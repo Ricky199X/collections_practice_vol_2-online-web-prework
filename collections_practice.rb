110c525952e80b8bc08ae1ba48ef5663fe6718b0
Ricky Rojas <rickyrojas19@gmail.com>
2019-04-07 18:19:56 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -58,3 +58,17 @@ def find_cool(array)
   array.select {|element| element if element.has_value?("cool")}
 end
 
+
+def organize_schools(schools)
+  by_location = {}
+    schools.each do |school, location_hash|
+      location_hash.each do |symbol, location|
+        if by_location[location] == nil
+          by_location[location] = [school]
+        else
+          by_location[location] << school
+        end
+      end
+    end
+    by_location
+end
