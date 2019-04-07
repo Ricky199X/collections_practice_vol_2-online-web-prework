6cfac595cb5de4561ba6a4e76b736e244050fd15
Ricky Rojas <rickyrojas19@gmail.com>
2019-04-07 16:04:58 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,8 +5,8 @@ end
 #contain_a: return all elements that contain the letter'a'
 
 def contain_a(array)
-  array.collect do {|element| element.include?("a")}
-    end
+  array.collect do |element| 
+    element.include?("a")
   end
 end
 
