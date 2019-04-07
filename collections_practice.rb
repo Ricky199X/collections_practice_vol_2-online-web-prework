4a06db261deabed74b3dcdafb2441da585866088
Ricky Rojas <rickyrojas19@gmail.com>
2019-04-07 16:01:35 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,6 +2,10 @@ def begins_with_r(array)
   array.all? {|string| string.start_with?("r")}
 end
 
-#contain_a: return all elements that contain the letter 'a'
+#contain_a: return all elements that contain the letter'a'
 
-def contain_a(array )
+def contain_a(array)
+  array_of_a = []
+  array.collect do |element|
+    if element.include?
+end
